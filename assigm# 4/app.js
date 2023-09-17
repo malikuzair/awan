876fe7d2529cfb307d2755b3c9e18df7a844da02
malikuzair <46522546+malikuzair@users.noreply.github.com>
2023-09-17 00:21:46 -0700

    Update app.js

---
+++ assigm# 4/app.js
@@ -0,0 +1,75 @@
+// ARRAY AND LOOPS
+// ++++++++++++++++Q1+++++++++++++
+
+// ++++++++++/ Q2++++++++++
+
+
+//++++++++++++++++++ Q3++++++++++++++++
+
+// for (var i=0; i <= 10; i++ ){
+//     console.log(i);
+//     document.write(i + "<br>")
+// }
+
+
+//=============== Q4 ================
+// var tableNumber= +prompt("1 enter the table number");
+// var tableLength= +prompt("2 enter the table length");
+
+// for (var i =0; i <= tableLength; i++){  
+//     console.log(tableNumber*i)
+//     document.write(tableNumber + "x" + i + "=" +tableNumber*i + "<br>")
+// }
+
+// // +++++++++++++++Q5++++++++++
+// var fruits =[ "apple", "mango", "banana" , "kiwi" ,"orange", "strawberry"]
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// // ++++++++++++Q6++++++
+// // Initialize an empty string to store the series
+// var series = "";
+
+// // Loop from 1 to 15 and add each number to the series string
+// for (var i = 1; i <= 15; i++) {
+//   // Append the current number to the series string
+//   series += i;
+
+//   // Add a comma and space after each number except the last one
+//   if (i < 15) {
+//     series += ", ";
+//   }
+// }
+
+// // Print the series to the console
+// console.log("Counting: " + series);
+
+
+// // q6 (A)
+// for (var i = 0; i<=15; i++) {
+//     console.log(i)
+//     document.write(i + "<br>")
+// }
+
+
+// // q6 (B)
+// for (var i = 20; i>=0; i--) {
+//     console.log(i)
+//     document.write(i + "<br>")
+// }
+
+// // q6 (c)
+// for (var i = 0; i<=15; i +=2) {
+//     console.log(i)
+//     document.write(i +"<br>")
+// }
+
+// q6(D)
+
+// var oddNumbers = "";
+// for (var i =1 ; i<= 21; i++){
+//     if ( i % 2 !== 0) {
+// oddNumbers += i + "," ;}
+//     }
+// document.write("odd numbers: " + oddNumbers.slice(0, -2));
